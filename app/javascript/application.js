11e18b58e8acc8060607b4b334c8e71d83d159d6
Marcos Nochebuena <mark_noche@hotmail.com>
2024-11-26 09:59:43 -0600

    Add select2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,18 +1,8 @@
 import "@hotwired/turbo-rails";  // Correcto: al nivel superior
 import "controllers";  // Asegúrate de que esté configurado correctamente
 import "@popperjs/core";
-import "bootstrap";  // Esto está bien, asegúrate de que se carga correctamente
-import Swal from 'sweetalert2';  // SweetAlert2 correctamente importado
-window.Swal = Swal;
+import "bootstrap";
 
-// Tu código personalizado aquí
-window.addEventListener('turbo:load', event => {
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
-    }
-});
+import "template/jquery-import";
+import "select2";
+import "template/template";
